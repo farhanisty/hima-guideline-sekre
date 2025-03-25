2420cfe68a40d2c2e4cf8d10d3c747802d106dee
farhanisty <farhannivtasija@gmail.com>
2025-03-25 23:13:49 +0700

    clean new src

--- src/components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-const Hero = () => {
-  return (
-  <header className="container mx-auto my-3">
-      <h1 className="uppercase text-3xl font-semibold">BIRO KESEKRETARIATAN KABINET GELORA HARMONI 2025</h1>
-  </header>
-  )
-}
-
-export default Hero;
