d835fea737ec848ec06cd7e1de394373f3cb9dd6
farhanisty <farhannivtasija@gmail.com>
2025-03-27 23:06:13 +0700

    create warning fragment

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import RegulationFragment from "./fragments/RegulationFragment"
 import TermsAndConditionFragment from "./fragments/TermsAndConditionFragment"
 import TimelineFragment from "./fragments/TimelineFragment"
 import HeroFragment from "./fragments/HeroFragment"
+import WarningFragment from "./fragments/WarningFragment"
 import EclipseImage from "./assets/eclipse.png"
 
 function App() {
   return (
-    <>
+    <div className="relative overflow-hidden">
       <img className="absolute" src={EclipseImage} alt="" />
       <HeroFragment />
       <ContactImportantFragment/>
@@ -16,7 +17,12 @@ function App() {
       <RegulationFragment />
       <TermsAndConditionFragment />
       <TimelineFragment />
-    </>
+      <WarningFragment />
+      <div className="absolute bg-red-200 w-[200%] bottom-[-50px] h-[480px] left-[-50px] rotate-[-5deg]" style={{
+        zIndex: -1,
+        background: "linear-gradient(var(--secondary), var(--primary))"
+      }}></div>
+    </div>
   )
 }
 
--- src/fragments/RegulationFragment.jsx
+++ src/fragments/RegulationFragment.jsx
@@ -1,15 +1,24 @@
 import Container from "../components/Container"
 import SectionTitle from "../components/SectionTitle"
+import Accordion from "../components/Accordion.jsx"
 
 export default function RegulationFragment() {
   return (
     <div className="py-[3rem]">
       <Container>
         <SectionTitle className="text-[2rem]">Regulations</SectionTitle>
-        <ul className="flex flex-col gap-5 mt-5">
-          <li className="w-full bg-secondary text-white p-3 rounded font-semibold">Pengajuan Dokumen</li>
-          <li className="w-full bg-secondary text-white p-3 rounded font-semibold">Pengajuan Dokumen</li>
-        </ul>
+        <div className="mt-5">
+            <Accordion>
+              <Accordion.Item className="bg-secondary text-white rounded font-semibold" value="data-1">
+                <Accordion.Trigger>Pengajuan Dokumen</Accordion.Trigger>
+                <Accordion.Body>hello worldkj</Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item className="bg-secondary text-white rounded font-semibold" value="data-2">
+                <Accordion.Trigger>Pengajuan Dokumen</Accordion.Trigger>
+                <Accordion.Body>hello worldkj</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+        </div>
       </Container>
     </div>
   )
