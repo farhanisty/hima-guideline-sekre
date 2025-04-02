a0923dc739e01f9d7d52df94a9e33e99a75ce3b3
farhanisty <farhannivtasija@gmail.com>
2025-04-02 11:01:53 +0700

    add group hover in board item

--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -1,13 +1,13 @@
 const BoardItem = ({children}) => {
   return (
-    <div className="bg-white rounded-xl p-5 hover:bg-secondary hover:text-white hover:-rotate-1 transition-transform">
+    <div className="group bg-white rounded-xl p-5 hover:bg-secondary hover:text-white hover:-rotate-1 transition-transform">
       {children}
     </div>
   )
 }
 
 BoardItem.Header = ({children}) => {
-  return <h2 className="font-semibold text-secondary text-lg">{children}</h2>
+  return <h2 className="group-hover:text-white font-semibold text-secondary text-lg">{children}</h2>
 }
 
 BoardItem.Body = ({children}) => {
