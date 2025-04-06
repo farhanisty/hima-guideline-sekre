0a4b785d4f3ab7d57f18a48d2c203aa13bc074f5
farhanisty <farhannivtasija@gmail.com>
2025-04-06 22:20:00 +0700

    undummy regulation fragment

--- src/fragments/RegulationFragment.jsx
+++ src/fragments/RegulationFragment.jsx
@@ -9,13 +9,27 @@ export default function RegulationFragment() {
         <SectionTitle className="text-[2rem]">Regulations</SectionTitle>
         <div className="mt-5">
             <Accordion>
-              <Accordion.Item className="bg-secondary text-white rounded font-semibold" value="data-1">
+              <Accordion.Item className="bg-secondary text-white rounded font-semibold" value="regulation-1">
                 <Accordion.Trigger>Pengajuan Dokumen</Accordion.Trigger>
-                <Accordion.Body>hello worldkj</Accordion.Body>
+                <Accordion.Body>
+                  <div className="pl-3">
+                    <ul className="list-decimal flex flex-col gap-2">
+                      <li>Pengajuan dokumen seperti surat-surat, proposal kegiatan, dan laporan pertanggungjawaban harus sesuai dengan tenggat waktu yang ditetapkan oleh Biro Kesekretariatan HIMASISFO.</li>
+                      <li>Semua pengajuan yang dilakukan di luar jam kerja tidak akan diproses. Pengajuan harus diajukan kembali saat jam kerja, kecuali jika ada keadaan mendesak.</li>
+                    </ul>
+                  </div>
+                </Accordion.Body>
               </Accordion.Item>
-              <Accordion.Item className="bg-secondary text-white rounded font-semibold" value="data-2">
-                <Accordion.Trigger>Pengajuan Dokumen</Accordion.Trigger>
-                <Accordion.Body>hello worldkj</Accordion.Body>
+              <Accordion.Item className="bg-secondary text-white rounded font-semibold" value="regulation-2">
+                <Accordion.Trigger>Pengarsipan Dokumen</Accordion.Trigger>
+                <Accordion.Body>
+                  <div className="pl-3">
+                    <ul className="list-decimal flex flex-col gap-2">
+                      <li>Semua dokumen kegiatan, termasuk proposal, surat-menyurat, dan Laporan Pertanggungjawaban (LPJ), harus diarsipkan secara digital dan/atau fisik.</li>
+                      <li>Pengarsipan dilakukan sesuai format yang telah ditetapkan oleh Biro Kesekretariatan dan harus tersedia untuk pengecekan kapan saja jika diperlukan.</li>
+                    </ul>
+                  </div>
+                </Accordion.Body>
               </Accordion.Item>
             </Accordion>
         </div>
